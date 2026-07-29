118e644b7551ecddbcef3d5630144cca6656f683
Sahil-09 <65083346+Sahil-09@users.noreply.github.com>
2026-07-29 11:28:48 +0530

    deployment fixes

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -1,12 +1,14 @@
 console.log("ℹ️ Prisma Config Loading...");
 console.log("ℹ️ DATABASE_URL in environment:", process.env.DATABASE_URL ? "RESOLVED (starts with " + process.env.DATABASE_URL.substring(0, 10) + ")" : "NOT RESOLVED (undefined)");
-console.log(process.env['DATABASE_URL']);
-export default {
+console.log(process.env['DATABASE_URL'])
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: encodeURI(process.env["DATABASE_URL"]),
+    url: env("DATABASE_URL"),
   },
-};
+});
